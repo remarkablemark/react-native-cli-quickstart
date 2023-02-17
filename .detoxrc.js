4989865b0001295f30a5fd7a62e27c4aa4240b79
Mark <remarkablemark@users.noreply.github.com>
2023-02-17 14:58:48 -0500

    chore(detoxrc): remove testBinaryPath

--- .detoxrc.js
+++ .detoxrc.js
@@ -34,7 +34,6 @@ module.exports = {
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
       build:
         'cd android ; ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd -',
-      testBinaryPath: 'android/app/build/outputs/apk/release/app-release-androidTest.apk',
     },
   },
   devices: {
