06e43649e5bfcd0f98e25dcbbed3033697c8061e
Mark <remarkablemark@users.noreply.github.com>
2023-02-18 14:52:20 -0500

    test(e2e): revert testTimeout back to 120000

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'react-native',
   rootDir: '..',
   testMatch: ['<rootDir>/e2e/**/*.test.ts'],
-  testTimeout: 5 * 60 * 1000, // 5 minutes
+  testTimeout: 120000,
   maxWorkers: 1,
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
